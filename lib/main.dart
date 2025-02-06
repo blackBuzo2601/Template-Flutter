d0b1cf7a4882aabef2580ebb6435022a6a865ba4
blackBuzo2601 <elianpro58@gmail.com>
2025-02-06 01:31:56 -0800

    Card para cada pelicula diseño listo

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MainApp extends StatelessWidget {
                   Container(
                     //Seccion TOP 10
                     width: double.infinity,
-                    height: 350,
+                    height: 420,
                     color: Colors.orange,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -58,9 +58,136 @@ class MainApp extends StatelessWidget {
                           child: Row(
                             children: [
                               Container(
+                                margin: EdgeInsets.only(left: 10),
                                 width: 150,
-                                height: 250,
+                                height: 380,
                                 color: Colors.black,
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      height: 200,
+                                      width: double.infinity,
+                                      child: Image.asset(
+                                        "assets/ahsoka.jpg",
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Container(
+                                        width: double.infinity,
+                                        height: 100,
+                                        color: const Color.fromARGB(
+                                            255, 49, 50, 49),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.star,
+                                                  color: const Color.fromARGB(
+                                                      255, 224, 210, 78),
+                                                ),
+                                                Text(
+                                                  "7.5",
+                                                  style: TextStyle(
+                                                      fontSize: 18,
+                                                      fontFamily:
+                                                          "Times New Roman",
+                                                      color:
+                                                          const Color.fromARGB(
+                                                              255,
+                                                              153,
+                                                              153,
+                                                              153),
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              ],
+                                            ),
+                                            Text(
+                                              textAlign: TextAlign.justify,
+                                              "Ahsoka Tano ",
+                                              style: TextStyle(
+                                                  fontFamily: "Roboto",
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20,
+                                                  color: const Color.fromARGB(
+                                                      247, 236, 234, 234)),
+                                            ),
+                                            SizedBox(
+                                              width: double.infinity,
+                                              height: 15,
+                                            ),
+                                            Expanded(
+                                              child: Container(
+                                                width: double.infinity,
+                                                height: double.infinity,
+                                                child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceAround,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.center,
+                                                  children: [
+                                                    Container(
+                                                      decoration: BoxDecoration(
+                                                        color: const Color
+                                                            .fromARGB(
+                                                            214, 93, 93, 93),
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                      width: 140,
+                                                      child: Text(
+                                                        textAlign:
+                                                            TextAlign.center,
+                                                        "Donde verla",
+                                                        style: TextStyle(
+                                                            fontFamily: "serif",
+                                                            color: Colors
+                                                                .blueAccent,
+                                                            fontSize: 18,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold),
+                                                      ),
+                                                    ),
+                                                    Container(
+                                                      decoration: BoxDecoration(
+                                                        color: const Color
+                                                            .fromARGB(
+                                                            214, 93, 93, 93),
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                      ),
+                                                      width: 140,
+                                                      child: Text(
+                                                        textAlign:
+                                                            TextAlign.center,
+                                                        "Trailer",
+                                                        style: TextStyle(
+                                                            fontFamily: "serif",
+                                                            color: Colors.white,
+                                                            fontSize: 18,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             ],
                           ),
