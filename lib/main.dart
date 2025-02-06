04156cfc8f19227e0abb3687c24df21f34104020
blackBuzo2601 <elianpro58@gmail.com>
2025-02-06 15:49:41 -0800

    reiniciado

--- lib/main.dart
+++ lib/main.dart
@@ -15,147 +15,20 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
-                centerTitle: true,
-                backgroundColor: Colors.black,
-                leading: Icon(
-                  Icons.theaters_rounded,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  textAlign: TextAlign.end,
-                  "KaelusTV Plus ",
-                  style: TextStyle(
-                      fontFamily: "Times New Roman",
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                )),
+              centerTitle: true,
+              backgroundColor: Colors.green,
+            ),
             body: Container(
               width: double.infinity,
               height: double.infinity,
-              color: Colors.black,
-              
-            )));
-  }
-}
-
-class SingleMovie extends StatelessWidget {
-  final String tituloPelicula;
-  final String estrellas;
-  final String rutaPelicula;
-
-  const SingleMovie({
-    required this.tituloPelicula,
-    required this.estrellas,
-    required this.rutaPelicula,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 10),
-      width: 150,
-      height: 380,
-      color: Colors.black,
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 15),
-            height: 200,
-            width: double.infinity,
-            child: Image.asset(
-              rutaPelicula,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: 100,
-              color: const Color.fromARGB(255, 49, 50, 49),
+              color: const Color.fromARGB(255, 136, 178, 213),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: const Color.fromARGB(255, 240, 255, 24),
-                      ),
-                      Text(
-                        estrellas,
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: "Times New Roman",
-                            color: const Color.fromARGB(255, 153, 153, 153),
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    textAlign: TextAlign.center,
-                    tituloPelicula,
-                    style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: const Color.fromARGB(247, 236, 234, 234)),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 15,
-                  ),
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(214, 93, 93, 93),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            width: 140,
-                            child: Text(
-                              textAlign: TextAlign.center,
-                              "Donde verla",
-                              style: TextStyle(
-                                  fontFamily: "serif",
-                                  color: Colors.blueAccent,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(214, 93, 93, 93),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            width: 140,
-                            child: Text(
-                              textAlign: TextAlign.center,
-                              "Trailer",
-                              style: TextStyle(
-                                  fontFamily: "serif",
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
+                  Text("hola"),
+                  Text("hola"),
+                  Text("hola"),
                 ],
               ),
-            ),
-          )
-        ],
-      ),
-    );
+            )));
   }
 }
